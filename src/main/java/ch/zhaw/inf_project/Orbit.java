e1e3c38cc884d94c8f4627bfc4d085ef9a2d051d
Philipp Schalcher <philipp.schalcher@phischa.ch>
2012-04-03 16:20:17 +0200

    Dokumentation eingefügt

--- src/main/java/ch/zhaw/inf_project/Orbit.java
+++ src/main/java/ch/zhaw/inf_project/Orbit.java
@@ -1,49 +1,101 @@
 package ch.zhaw.inf_project;
 
+/**
+ * Klasse symbolisiert den Orbit des Satelliten
+ * 
+ * @author Team 1
+ *
+ */
+
 public class Orbit {
 	private int posx,posy,radx,rady;
+	
+	/**
+	 * Gibt X-Koordinate zurück
+	 * @return posx
+	 */
 	public int getPosx() {
 		return posx;
 	}
 
+	/**
+	 * Setzt X-Koordinate neu
+	 * @param posx
+	 */
 	public void setPosx(int posx) {
 		this.posx = posx;
 	}
 
+	/**
+	 * gibt Y-Koordinate zurück
+	 * @return posy
+	 */
 	public int getPosy() {
 		return posy;
 	}
 
+	/**
+	 * Setzt Y-Koordinate neu
+	 * @param posy
+	 */
 	public void setPosy(int posy) {
 		this.posy = posy;
 	}
 	
+	/**
+	 * Gibt Radius in Y-Richtung zurück
+	 * @return rady
+	 */
 	public int getRady() {
 		return rady;
 	}
 
+	/**
+	 * Setzt den Radius in Y-Richtung neu
+	 * @param rady
+	 */
 	public void setRady(int rady) {
 		this.rady = rady;
 	}
 	
+	/**
+	 * Gibt den Radius in X-Richtung zurück
+	 * @return radx
+	 */
 	public int getRadx() {
 		return radx;
 	}
 
+	/**
+	 * Setzt den Radius in X-Richtung neu
+	 * @param radx
+	 */
 	public void setRadx(int radx) {
 		this.radx = radx;
 	}
 
+	/**
+	 * Gibt Farbe zurück
+	 * @return color
+	 */
 	public String getColor() {
 		return color;
 	}
 
+	/**
+	 * Setzt Farbe neu
+	 * @param color
+	 */
 	public void setColor(String color) {
 		this.color = color;
 	}
 
 	private String color = "black";
 	
+	/**
+	 * Konstruktor
+	 * Initialisiert Variablen
+	 */
 	public Orbit()
 	{
 		this.posx = 100;
